f28639cca74dd6fdd404b775629d355085d4ef92
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-06 21:02:21 -0500

    Dibujo y funcion graficarRectangulo() v2 final

--- cazando.js
+++ cazando.js
@@ -1,34 +1,37 @@
 
 const canvas = document.getElementById("areaJuego");
 const ctx = canvas.getContext("2d");
-
+ 
 let gatoX = 0;
 let gatoY = 0;
 let comidaX = 0;
 let comidaY = 0;
-
+ 
 const ANCHO_GATO = 40;
 const ALTO_GATO = 40;
 const ANCHO_COMIDA = 20;
 const ALTO_COMIDA = 20;
-
+ 
+function graficarRectangulo(x, y, ancho, alto, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, ancho, alto);
+}
+ 
 function graficarGato() {
-  ctx.fillStyle = "green";
-  ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+  graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "green");
 }
-
+ 
 function graficarComida() {
-  ctx.fillStyle = "red";
-  ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "red");
 }
-
+ 
 function iniciarJuego() {
   gatoX = (canvas.width - ANCHO_GATO) / 2;
   gatoY = (canvas.height - ALTO_GATO) / 2;
-
-  comidaX = canvas.width - ANCHO_COMIDA;
+ 
+ comidaX = canvas.width - ANCHO_COMIDA;
   comidaY = canvas.height - ALTO_COMIDA;
-
+ 
   graficarGato();
   graficarComida();
 }
