c9adc5b412223b6309bece4b4a1d5fe33a2b7981
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-06 20:45:22 -0500

    Agregar comida e iniciarJuego()

--- cazando.js
+++ cazando.js
@@ -2,8 +2,17 @@
 const canvas = document.getElementById("areaJuego");
 const ctx = canvas.getContext("2d");
 
-
 function graficarGato() {
   ctx.fillStyle = "green";
   ctx.fillRect(230, 230, 40, 40);
+}
+
+function graficarComida() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 20, 20);
+}
+
+function iniciarJuego() {
+  graficarGato();
+  graficarComida();
 }
