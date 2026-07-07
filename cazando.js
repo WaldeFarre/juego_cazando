f404539a9eb5dd8683a23c58dff8d896c0405f6a
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-06 20:43:23 -0500

    Dibujar al gato en el canvas con la funcion graficarGato()

---
+++ cazando.js
@@ -0,0 +1,9 @@
+
+const canvas = document.getElementById("areaJuego");
+const ctx = canvas.getContext("2d");
+
+
+function graficarGato() {
+  ctx.fillStyle = "green";
+  ctx.fillRect(230, 230, 40, 40);
+}
